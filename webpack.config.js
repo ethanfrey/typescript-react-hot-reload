0065098f37d77e425b23bd3cd84ea2755cd9c3f8
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-10-06 11:38:39 +0200

    Disable webpack plugin that fails on my machine

--- webpack.config.js
+++ webpack.config.js
@@ -101,6 +101,6 @@ module.exports = {
         // prints more readable module names in the browser console on HMR updates
         new HtmlWebpackPlugin({template: resolve(__dirname, 'src/index.html')}),
         // inject <script> in html file. 
-        new OpenBrowserPlugin({url: 'http://localhost:8080'}),
+        // new OpenBrowserPlugin({url: 'http://localhost:8080'}),
     ],
 };
